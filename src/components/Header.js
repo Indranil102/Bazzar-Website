d86ec17b2586ba3719db98f360d5cf68774ac61b
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-23 21:31:28 +0530

    nav added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { logoDark } from '../assets'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='w-full h-20 bg-white border-b-[1px] border-b-gray-800'>
+      <div className='max-w-screen-xl h-full mx-auto flex items-center justify-between'>
+        <div>
+            <img className='w-28' src={logoDark} alt=''/>
+        </div>
+
+        <div>
+        <ul className='flex items-center gap-8'>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer'>Home</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer'>Page</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer'>Shop</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer'>Element</li>
+            <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer'>Blog</li>
+
+        </ul>
+      </div>
+      </div>
+     
     </div>
   )
 }
