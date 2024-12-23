1c88e01294a9de7c56cae062ad2f0b9bd5336920
Indranil102 <indranilsamanta2003@gmail.com>
2024-12-23 17:23:20 +0530

    asset update

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import logoDark from './logoDark.png';
+import logoLight from './logoLight.png';
+import cartImg from'./cart.png';
+import paymentLogo from './paymentLogo.png';
+import googlelogo from './googleLogo.png';
+import githubLogo from './githubLogo.png';
+export{logoDark, logoLight,cartImg,paymentLogo,googlelogo, githubLogo};
